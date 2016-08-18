45dcff66e3fde70747ce893136a26d7e231a51a7
Robert <rharrison@hgf.com>
2016-08-18 19:12:07 +0000

    Added CURLOPT_FAILONERROR to capture http errors on habhub these will now be reported via log message at the botom of the window

--- habitat.c
+++ habitat.c
@@ -78,6 +78,9 @@ void UploadTelemetryPacket(int Channel)
 		
 		// Set the timeout
 		curl_easy_setopt(curl, CURLOPT_TIMEOUT, 5);
+
+                // RJH capture http errors and report
+                curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1);
 		
 		// Avoid curl library bug that happens if above timeout occurs (sigh)
 		curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
